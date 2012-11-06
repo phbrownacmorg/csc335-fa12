0007537eb1241d4bc407cf3ead916280b5f82512
Peter Brown <phbrown@acm.org>
2012-11-05 20:05:40 -0500

    Another change to git-autopull.php, delegating the actual pull to a cron job. git-autopull.php now only creates a flag file, which is a lot easier to debug.

--- git-autopull.php
+++ git-autopull.php
@@ -1,5 +1,20 @@
 <?php 
+try
+{
+  $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
 
-system('/usr/bin/git pull -u https://github.com/phbrownacmorg/csc335-fa12.git');
+//log the request
+file_put_contents('/tmp/github.txt', print_r($payload, TRUE));
+file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
+if ($payload->ref === 'refs/heads/master')
+{
+  // path to your site deployment script
+  //exec('./build.sh');
+}
 ?>
