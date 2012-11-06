8a1f81a1c2f9891db7959829156ebe580ed2c190
Peter Brown <phbrown@acm.org>
2012-11-05 23:03:54 -0500

    Changed location of git-autpull's logs

--- git-autopull.php
+++ git-autopull.php
@@ -10,13 +10,13 @@ catch(Exception $e)
 
 //log the request
 file_put_contents('/tmp/github.txt', print_r($payload, TRUE));
-file_put_contents('/tmp/logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+file_put_contents('/tmp/logs-github.txt', print_r($payload, TRUE), FILE_APPEND);
 
 if ($payload->ref === 'refs/heads/master')
 {
-  file_put_contents('/tmp/logs/github.txt', 'Pulling', FILE_APPEND);
+  file_put_contents('/tmp/logs-github.txt', 'Pulling', FILE_APPEND);
   // path to your site deployment script
-  file_put_contents('/tmp/logs/github.txt', shell_exec('git pull'), FILE_APPEND);
+  file_put_contents('/tmp/logs-github.txt', shell_exec('git pull'), FILE_APPEND);
 }
 ?>
 
